cfce712a6aec497cad8be15579823679750572eb
kremrik <kyle.robert.e@gmail.com>
2022-03-28 21:24:41 -0500

    added stdin iterator

--- cli_static/src/io.rs
+++ cli_static/src/io.rs
@@ -5,6 +5,7 @@ use std::io::{
     BufRead,
     BufReader,
     Lines,
+    stdin,
 };
 
 
@@ -27,6 +28,30 @@ impl<'b> Iterator for FileIterator<'b> {
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(line) = self.buffer.next() {
             return Some(line.unwrap())
+        }
+
+        None
+    }
+}
+
+
+pub struct StdinIterator {}
+
+impl StdinIterator {
+    pub fn new() -> StdinIterator {
+        StdinIterator {}
+    }
+}
+
+impl Iterator for StdinIterator {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buffer = String::new();
+        let data = stdin().read_line(&mut buffer).unwrap();
+
+        if data > 0 {
+            return Some(buffer)
         }
 
         None
--- cli_static/src/main.rs
+++ cli_static/src/main.rs
@@ -1,17 +1,27 @@
 #![allow(dead_code)]
 
 use htmlayout::cli::make_help;
-use htmlayout::io::FileIterator;
+use htmlayout::io::{
+    FileIterator,
+    StdinIterator,
+};
 
 use std::fs::File;
+use std::io::stdin;
 
 
 fn main() {
-    let path = "/home/kyle/projects/html-template/templates/base.html";
+    // let path = "/home/kyle/projects/html-template/templates/base.html";
 
-    let file = File::open(path).unwrap();
-    let fi = FileIterator::new(&file);
+    // let file = File::open(path).unwrap();
+    // let fi = FileIterator::new(&file);
 
-    let help = make_help(fi);
-    println!("{}", help);
+    // let help = make_help(fi);
+    // println!("{}", help);
+
+
+    let si = StdinIterator::new();
+    for line in si {
+        print!("> {}", line);
+    }
 }
